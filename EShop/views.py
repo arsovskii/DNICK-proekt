4a7cc83de0d6e841abd3fc84cfe42a634ee353ab
David <arsovskidavid4@gmail.com>
2023-06-25 15:43:59 +0200

    fix

--- EShop/views.py
+++ EShop/views.py
@@ -43,7 +43,7 @@ def register(request):
                         password=hashers.make_password(form_data.cleaned_data["password"])
                         )
             send_mail("Креиран е нов профил", f"Креиран е профил со корисничко име {user.username}",
-                       [user.email])
+                      None, [user.email])
             user.save()
             buyerUser = BuyerUser(user=user)
             buyerUser.save()
